30b1f20541793c8c4600168780e2b85daa530647
nnnyt <793313994@qq.com>
2021-01-22 17:57:11 +0800

    add D-opt strategy for MIRT, unify D-opt strategy and MFI strategy

--- CAT/strategy/MFI_strategy.py
+++ CAT/strategy/MFI_strategy.py
@@ -7,23 +7,35 @@ from CAT.dataset import AdapTestDataset
 
 
 class MFIStrategy(AbstractStrategy):
+    """
+    Maximum Fisher Information Strategy
+    D-opt Strategy when using MIRT(num_dim != 1)
+    """
 
     def __init__(self):
         super().__init__()
+        self.I = None
 
     @property
     def name(self):
         return 'Maximum Fisher Information Strategy'
 
     def adaptest_select(self, model: AbstractModel, adaptest_data: AdapTestDataset):
-        assert hasattr(model, 'get_iif'), \
-            'the models must implement get_iif method'
+        assert hasattr(model, 'get_fisher'), \
+            'the models must implement get_fisher method'
+        if self.I is None:
+            self.I = [np.zeros((model.model.num_dim, model.model.num_dim))] * adaptest_data.num_students    
         selection = {}
+        n = len(adaptest_data.tested[0])
         for sid in range(adaptest_data.num_students):
             untested_questions = np.array(list(adaptest_data.untested[sid]))
-            untested_iif = []
+            untested_dets = []
+            untested_fisher = []
             for qid in untested_questions:
-                untested_iif.append(model.get_iif(sid, qid))
-            j = np.argmax(untested_iif)
+                fisher_info = model.get_fisher(sid, qid)
+                untested_fisher.append(fisher_info)
+                untested_dets.append(np.linalg.det(self.I[sid] + fisher_info))
+            j = np.argmax(untested_dets)
             selection[sid] = untested_questions[j]
+            self.I[sid] += untested_fisher[j]
         return selection
