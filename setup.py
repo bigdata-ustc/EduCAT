6bc794cb290ee41ffc79d87f978a289b8eb0a7dd
Yuting Ning <48355377+nnnyt@users.noreply.github.com>
2024-01-24 16:29:42 +0800

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CAT',
+    name='EduCAT',
     version='0.0.1',
     author='Yuting Ning',
     author_email='ningyt@mail.ustc.edu.cn',
